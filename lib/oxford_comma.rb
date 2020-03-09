c5a1d0ee9b6f84ff787ca04ea22ed8d09390b887
Wielfried Zouantcha <zouantchaw74@gmail.com>
2020-03-09 18:44:23 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-array.join(",")
+  if array.length == 2
+    array.join(" and ")
+  elsif array.length == 3 || array.length > 3
+    lastitem = array[-1]
+    array[-1] = "and #{lastitem}"
+    array.join(", ")
+  end
 end
